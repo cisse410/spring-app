2ff7a1adf6e507badc6e5649be15324e14e3d64b
cisse410 <dramisse90@gmail.com>
2024-08-02 04:47:38 +0000

    Project setup

---
+++ src/test/java/sn/cisse410/spring_crud_app/SpringCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.cisse410.spring_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
